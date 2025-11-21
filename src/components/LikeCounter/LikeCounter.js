5048484ef41ea623ba184186e23e86b6e4241f75
Adrian Widlak <awidlak@adrianwii.pl>
2025-11-21 17:55:59 +0100

    first commit

---
+++ src/components/LikeCounter/LikeCounter.js
@@ -0,0 +1,12 @@
+import './LikeCounter.css';
+
+function LikeCounter() {
+  return (
+    <div className='post'>
+      <p>Fajnyyyy post!! Liczba lików X</p>
+      <button>Polub 👍</button>
+    </div>
+  );
+}
+
+export default LikeCounter;
